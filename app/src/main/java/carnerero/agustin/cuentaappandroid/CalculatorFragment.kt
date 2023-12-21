a2fdb275d5ae8007177cc14ea0aee4cf86b72096
Agustin Carnerero <agusticar@gmail.com>
2023-12-21 15:47:05 +0100

    consigo resolver el formato de punto en la calculadora...genial!!!

--- app/src/main/java/carnerero/agustin/cuentaappandroid/CalculatorFragment.kt
+++ app/src/main/java/carnerero/agustin/cuentaappandroid/CalculatorFragment.kt
@@ -15,21 +15,10 @@ import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
 import java.util.Locale
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CalculatorFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 //Para gestionar los eventos de los botones de la calculadora implementamos View.OnClickListener
 class CalculatorFragment : Fragment(), View.OnClickListener {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     private val decimalFormat =  DecimalFormat("#,##0.00", DecimalFormatSymbols.getInstance(Locale.GERMANY))
 
@@ -39,8 +28,7 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -77,33 +65,12 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
                 }
 
             }
-        }
 
+        }
+    operation.addTextChangedListener(SeparateThousands(".",","))
         return view
     }
 
-
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CalculatorFragment.
-         */
-
-
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CalculatorFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 
     override fun onClick(view: View?) {
 
--- app/src/main/java/carnerero/agustin/cuentaappandroid/SeparateThousands.kt
+++ app/src/main/java/carnerero/agustin/cuentaappandroid/SeparateThousands.kt
@@ -1,63 +1,65 @@
 package carnerero.agustin.cuentaappandroid
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.TextView
+import java.util.StringTokenizer
 
 class SeparateThousands(val groupingSeparator: String, val decimalSeparator: String) : TextWatcher {
 
     private var busy = false
 
     override fun afterTextChanged(s: Editable?) {
-        // Verifica que el texto no sea nulo y que no estemos en medio de un procesamiento anterior
         if (s != null && !busy) {
-            // Establece la bandera busy para indicar que estamos procesando el texto
             busy = true
 
-            // Inicializa una variable para llevar la cuenta de la posición del dígito actual
-            var place = 0
+            var placeBefore = 0
+            var placeAfter = 0
+            var operatorFound = false
 
-            // Obtiene la posición del separador decimal en el texto
             val decimalPointIndex = s.indexOf(decimalSeparator)
-
-            // Inicializa i con la posición del dígito más a la derecha antes del separador decimal
-            // o el último dígito si no hay separador decimal
             var i = if (decimalPointIndex == -1) {
                 s.length - 1
             } else {
                 decimalPointIndex - 1
             }
 
-            // Inicia un bucle que recorre los dígitos del número desde la posición i hasta el inicio del texto
             while (i >= 0) {
-                // Obtiene el carácter en la posición actual
                 val c = s[i]
 
-                // Elimina el carácter si es el separador de millares
-                if (c == groupingSeparator[0]) {
-                    s.delete(i, i + 1)
+                if (c == '+' || c == '-' || c == '÷' || c == '×') {
+                    // Si se encuentra un operador, se indica y se reinicia la cuenta de dígitos
+                    operatorFound = true
+                    placeBefore = 0
+                } else if (c == groupingSeparator[0]) {
+                    s.delete(i, i + 1) // Se elimina el separador de millares
                 } else {
-                    // Inserta el separador de millares (punto) a la izquierda de cada tercer dígito,
-                    // siempre que no sea el dígito más a la izquierda
-                    if (place % 3 == 0 && place != 0) {
+                    if (!operatorFound && placeBefore % 3 == 0 && placeBefore != 0) {
+                        s.insert(i + 1, groupingSeparator)
+                    } else if (operatorFound && placeAfter % 3 == 0 && placeAfter != 0) {
                         s.insert(i + 1, groupingSeparator)
                     }
 
-                    // Incrementa la posición del dígito actual
-                    place++
+                    if (!operatorFound) {
+                        placeBefore++
+                    } else {
+                        placeAfter++
+                    }
                 }
 
-                // Mueve la posición i al siguiente dígito hacia la izquierda
                 i--
             }
 
-            // Restablece la bandera busy para indicar que el procesamiento ha terminado
             busy = false
         }
     }
 
-
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        // No se realiza ninguna acción antes de que el texto cambie
     }
 
     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        // No se realiza ninguna acción mientras el texto está cambiando
     }
 }
+
+
