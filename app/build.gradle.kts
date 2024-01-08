631be532a1594ca31a49377b0e3b76f1e3fe79d7
Agustin Carnerero <agusticar@gmail.com>
2024-01-08 17:11:43 +0100

    mejoras propuestas por google stores de contraste,etc.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "carnerero.agustin.cuentaappandroid"
         minSdk = 33
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.2.2"
+        versionCode = 6
+        versionName = "1.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
