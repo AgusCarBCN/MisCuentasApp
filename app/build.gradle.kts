019aeec1545e99861eb7a57451f4cce305c46b26
Agustin Carnerero <agusticar@gmail.com>
2024-01-08 18:18:47 +0100

    version 1.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "carnerero.agustin.cuentaappandroid"
         minSdk = 33
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.2.3"
+        versionCode = 7
+        versionName = "1.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
