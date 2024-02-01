5a9b0966375c33ef36e5e8c316bd3bb3f83f7457
Agustin Carnerero Peña <agusticar@gmail.com>
2024-02-01 22:18:41 +0100

    acabo version 1.3.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "carnerero.agustin.cuentaappandroid"
         minSdk = 26
         targetSdk = 34
-        versionCode = 22
-        versionName = "1.3.0"
+        versionCode = 26
+        versionName = "1.3.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
