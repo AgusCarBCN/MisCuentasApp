7b281c3ad0b444e548e48be1c1986afd5afbc797
Agustin Carnerero Peña <agusticar@gmail.com>
2024-11-15 21:08:17 +0100

    preparado para merge

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "carnerero.agustin.cuentaappandroid"
         minSdk = 27
         targetSdk = 34
-        versionCode = 35
-        versionName = "3.1"
+        versionCode = 36
+        versionName = "3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
