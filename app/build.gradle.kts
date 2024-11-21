d52070f95101baa036bd9a4a9184318af1a2c330
Agustin Carnerero Peña <agusticar@gmail.com>
2024-11-21 20:13:17 +0100

    version 3.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "carnerero.agustin.cuentaappandroid"
         minSdk = 27
         targetSdk = 34
-        versionCode = 41
-        versionName = "3.7"
+        versionCode = 42
+        versionName = "3.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
